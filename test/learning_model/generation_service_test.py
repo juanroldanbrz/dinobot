557d89473cd51421b2e0e93159c4c6bc537d912c
Juan Bermudez Roldan <juan.roldan.brz@gmail.com>
2019-10-18 08:32:08 +0200

    3rd generation stable and 2nd phase

--- test/learning_model/generation_service_test.py
+++ test/learning_model/generation_service_test.py
@@ -5,7 +5,7 @@ from service import generation_service
 
 class GenerationServiceTest(unittest.TestCase):
     def test_create_generation_report(self):
-        generation_service.create_generation_report(1)
+        generation_service.create_generation_report(2)
 
     def test_reproduce_generation(self):
         generation_service.reproduce_generation(1)
